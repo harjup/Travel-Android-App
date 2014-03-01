ac2dbbce52805575ce1c9d05935dec58fec8d4c5
KDhyne <kdhyne@gmail.com>
2014-03-01 14:23:11 -0500

    Refactor TripSettingsEditFrag and SummaryFrag
    GetNewExchangeRate function encapsulates call to Open Exchange Rates

--- src/com/harjup_kdhyne/TravelApp/MySQLiteHelper.java
+++ src/com/harjup_kdhyne/TravelApp/MySQLiteHelper.java
@@ -12,7 +12,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper
 {
     //Database constants
     private static final String DATABASE_NAME = "TravelApp.db";
-    private static final int DATABASE_VERSION = 7;
+    private static final int DATABASE_VERSION = 8;
 
     //Finances table constants
     public static final String PURCHASES_TABLE = "purchases";
